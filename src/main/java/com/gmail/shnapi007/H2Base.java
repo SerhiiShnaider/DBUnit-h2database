0a65f5df49ec1a500be0639de0d902840091dddd
Serhii_Shnaider <Serhii_Shnaider@epam.com>
2019-05-29 16:16:56 +0300

    Small fix

--- src/main/java/com/gmail/shnapi007/H2Base.java
+++ src/main/java/com/gmail/shnapi007/H2Base.java
@@ -1,9 +1,9 @@
 package com.gmail.shnapi007;
 
-public abstract class H2Base {
+ abstract class H2Base {
 
-  protected static final String JDBC_DRIVER = "org.h2.Driver";
-  protected static final String DB_URL = "jdbc:h2:~/test";
-  protected static final String USER = "sa";
-  protected static final String PASS = "";
+   static final String JDBC_DRIVER = "org.h2.Driver";
+   static final String DB_URL = "jdbc:h2:~/test";
+   static final String USER = "sa";
+   static final String PASS = "";
 }
--- src/main/java/com/gmail/shnapi007/H2Read.java
+++ src/main/java/com/gmail/shnapi007/H2Read.java
@@ -16,18 +16,17 @@ public class H2Read extends H2Base {
 
       try (Statement stmt = conn.createStatement()) {
         String sql = "SELECT id, first, last, age FROM USER";
-        ResultSet rs = stmt.executeQuery(sql);
+        try (ResultSet rs = stmt.executeQuery(sql)) {
 
-        while (rs.next()) {
+          while (rs.next()) {
 
-          System.out.print("ID: " + rs.getInt("id"));
-          System.out.print(", Age: " + rs.getInt("age"));
-          System.out.print(", First: " + rs.getString("first"));
-          System.out.println(", Last: " + rs.getString("last"));
+            System.out.print("ID: " + rs.getInt("id"));
+            System.out.print(", Age: " + rs.getInt("age"));
+            System.out.print(", First: " + rs.getString("first"));
+            System.out.println(", Last: " + rs.getString("last"));
+          }
         }
-        rs.close();
       }
-
     } catch (Exception e) {
       e.printStackTrace();
     }
